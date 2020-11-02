0eb972bbed24cd43fcc83b216f0144888dc7ae4c
YasuhikoHAYASHI <planair@gmail.com>
2020-11-02 11:10:23 +0900

    initial

---
+++ Assets/IndieMarc/TopDownDemo/Scripts/TimeScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class TimeScript : MonoBehaviour 
+{
+	private float time = 60;
+	public GameObject gameOverText;
+	public AudioClip endGame;
+	private AudioSource audio_source;
+
+		void Start () 
+		{
+			gameOverText.SetActive(false);
+			GetComponent<Text>().text = ((int)time).ToString();
+		}
+
+		void Update ()
+		{
+			time -= Time.deltaTime;
+			GetComponent<Text> ().text = ((int)time).ToString ();
+			//Debug.Log(time);
+			if (time < 0) 
+			{
+				//Debug.Log("StartCoroutine");
+				StartCoroutine(GameOver());
+
+				if (time < 0) time = 0;
+					GetComponent<Text> ().text = ((int)time).ToString ();
+					//Debug.Log("GetComponent");
+					IEnumerator GameOver() 
+					{
+						//Debug.Log("IEnumerator");
+						gameOverText.SetActive(true);
+						//audio_source.Play();
+						yield return new WaitForSeconds(2.0f);
+						Application.LoadLevel ("title");
+					}
+			}	
+		}
+}
---
+++ Assets/IndieMarc/TopDownDemo/Scripts/exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+
+public class exit : MonoBehaviour
+{
+  async void OnTriggerEnter2D(Collider2D col)
+   	{
+   		if (col.gameObject.tag == "Player")//ゴールのオブジェクトに接触したらクリアシーンへの切り替え
+   	  {
+              Debug.Log("touch!");
+              GetComponent<AudioSource>().Play(); 
+              await Task.Delay (1300);
+              SceneManager.LoadScene("TopDownDemo2");
+		  }
+    }
+}
